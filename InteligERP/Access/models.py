fa58927143e45ac22f81bdbc253b6f5eb7c7df93
brengodoy <dia.godoybrenda@gmail.com>
2023-07-04 11:55:29 -0300

    agregue los atributos de la clase user

--- InteligERP/Access/models.py
+++ InteligERP/Access/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class User(models.Model):
+    mail = models.CharField(max_length=200)
+    password = models.CharField(max_length=30)
+    date_created = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    dni = models.IntegerField()
+    docket = models.IntegerField()
+    birth_date = models.DateTimeField()
