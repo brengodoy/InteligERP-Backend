6e8c257cfdc8f6a52b508f50bfbb9d7452d7aa28
Cristian Gofiar <cristiangofiar@gmail.com>
2023-07-07 19:38:47 -0300

    views develop started

--- InteligERP/Access/admin.py
+++ InteligERP/Access/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+"""
+Este archivo se utiliza para registrar los 
+modelos de tu aplicación en el panel de 
+administración de Django. Puedes personalizar y
+controlar cómo se muestran y se manipulan los 
+datos de los modelos en la interfaz de administración. 
+Puedes registrar modelos específicos y configurar 
+campos para ser mostrados, filtrados, buscados y 
+ordenados en el panel de administración.
+"""
+
+
 # Register your models here.
--- InteligERP/Access/models.py
+++ InteligERP/Access/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+"""
+Este archivo es donde defines los modelos de tu 
+aplicación. Los modelos son clases que representan 
+las tablas de la base de datos y definen la estructura 
+y el comportamiento de los datos almacenados. Aquí 
+puedes definir los campos, relaciones, métodos y 
+ropiedades de tus modelos. Django utiliza esta 
+información para generar y administrar la estructura 
+de la base de datos, así como para realizar operaciones 
+CRUD (crear, leer, actualizar, eliminar) en los datos.
+"""
+
+
 # Create your models here.
