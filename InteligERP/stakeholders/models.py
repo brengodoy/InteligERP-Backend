ce76b1650cde139f68f519dcf24affe562708590
brengodoy <dia.godoybrenda@gmail.com>
2023-08-19 23:11:57 -0300

    app de stakeholders, modelos de Client y Supplier

---
+++ InteligERP/stakeholders/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Client(models.Model):
+    # Campos del modelo para Client
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+    CUIL = models.IntegerField(max_length=11)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Supplier(models.Model):
+    # Campos del modelo para Supplier
+    company_name = models.CharField(max_length=255)
+    CUIT = models.IntegerField(max_length=11)
+
+    def __str__(self):
+        return self.company_name
