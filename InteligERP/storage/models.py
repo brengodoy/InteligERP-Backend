328ced3f8be161bc53b646336ca6ba8809c8bfbb
brengodoy <dia.godoybrenda@gmail.com>
2023-10-31 20:04:41 -0300

    creé el modelo de warehouse

--- InteligERP/storage/models.py
+++ InteligERP/storage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Warehouse(models.Model):
+	id_warehouse = models.IntegerField(max_length=100)
+	name = models.CharField(max_length=300)
+	address = models.CharField(max_length=400)
+	description = models.CharField(max_length=400)
