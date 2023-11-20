0e138e2cb7983671cbf5125f8e1cce2c3633dfc7
Martin Ballestero <martin.ballestero@hotmail.com>
2023-11-20 11:10:36 -0300

    Use lowercase for Access module.

---
+++ InteligERP/access/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from .models import User
+
+"""
+Este archivo se utiliza para registrar los 
+modelos de tu aplicación en el panel de 
+administración de Django. Puedes personalizar y
+controlar cómo se muestran y se manipulan los 
+datos de los modelos en la interfaz de administración. 
+Puedes registrar modelos específicos y configurar 
+campos para ser mostrados, filtrados, buscados y 
+ordenados en el panel de administración.
+"""
+
+
+# Register your models here.
+admin.site.register(User)
---
+++ InteligERP/access/management/commands/create_users.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from access.models import User
+
+class Command(BaseCommand):
+    help = 'Crea usuarios de prueba'
+
+    def handle(self, *args, **kwargs):
+        usuario = User.objects.get(username='tinchoabc')
+        usuario.delete()
+        User.objects.create_user(
+            'tinchoabc', 'mb@mail.com', 'tincho123',
+            is_superuser=1, first_name='Martin', last_name='Ballestero'
+        )
+        self.stdout.write(self.style.SUCCESS('Usuarios creados exitosamente'))
